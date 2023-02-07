f28a7e5554ee2688b9e2dff4718cce49ab3ccb62
austenoooo <yl5661@nyu.edu>
2023-02-07 12:44:11 -0500

    implement Coco-SSD object detection

--- Week_2_assignment/script.js
+++ Week_2_assignment/script.js
@@ -1,22 +1,25 @@
 // Tutorials I followed: https://codelabs.developers.google.com/codelabs/tensorflowjs-object-detection#0
 
-// store hte resulting model in the global scope of our app;
-var model = undefined;
-
-cocoSsd.load().then(function (loadedModel){
-  model = loadedModel;
-  
-});
-
-
 const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
 const demosSection = document.getElementById('demos');
-const enableWebcamButton = document.getElementById('webcamButton');
+
+// store hte resulting model in the global scope of our app;
+var model = undefined;
+// var model = true;
+// demosSection.classList.remove('invisible');
+
+cocoSsd.load().then(function (loadedModel){
+  model = loadedModel;
+  // Show demo section now model is ready to use
+  demosSection.classList.remove('invisible');
+});
+
 
 // check if webcam access is supported
 function getUserMediaSupported() {
   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia); // what does !! mean here?
+}
 
 // If webcam supported, load the camera
 if (getUserMediaSupported()){
@@ -29,11 +32,12 @@ else{
 function enableCam(event){
   // only continue if the COCO-SSD has finished loading
   if (!model){
+    console.log("model cannot be loaded")
     return;
   }
 
   const constraints = {
-    video = true
+    video: true
   };
 
   // Activate the webcam stream
@@ -43,7 +47,43 @@ function enableCam(event){
   });
 }
 
-function predictWebcam(){
+var children = [];
 
+function predictWebcam(){
+  model.detect(video).then(function (predictions) {
+
+    // Remove any highlighting we did previous frame
+    for (let i = 0; i < children.length; i++){
+      liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+
+    // loop through predictions and draw them to the live view if they have a high confidence score
+    for (let n = 0; n < predictions.length; n++){
+      if (predictions[n].score > 0.3) {
+        const p = document.createElement('p');
+        p.innerText = predictions[n].class + ' - with '
+          + Math.round(parseFloat(predictions[n].score) * 100)
+          + '% confidence.';
+        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+          + (predictions[n].bbox[1] - 10) + 'px; width: '
+          + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+        
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', 'highlighter');
+        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+          + predictions[n].bbox[1] + 'px; width: '
+          + predictions[n].bbox[2] + 'px; height: '
+          + predictions[n].bbox[3] + 'px;';
+        
+        liveView.appendChild(highlighter);
+        liveView.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+    
+    window.requestAnimationFrame(predictWebcam);
+  });
 }
 
